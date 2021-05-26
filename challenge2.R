5aeebbc2e2e7c98baa6525e9e85a5155dd62def2
Gonzalo <gonzalo.fce@gmail.com>
2021-05-26 18:07:29 +0200

    edit group_by year AND age add rbind all countries add plot TFR by country and year

--- challenge2.R
+++ challenge2.R
@@ -54,46 +54,46 @@ kor_exposure <- readHFDweb(CNTRY = "KOR",
 # Spain
 spain_birth2 <- spain_birth %>% 
                   filter(OpenInterval == FALSE) %>% 
-                  group_by(Year) %>% 
+                  group_by(Year, Age) %>% 
                   summarise(Births = sum(Total, na.rm = TRUE))
 
 spain_exposure2 <- spain_exposure %>% 
                   filter(OpenInterval == FALSE) %>% 
-                  group_by(Year) %>% 
+                  group_by(Year, Age) %>% 
                   summarise(Exposure = sum(Exposure, na.rm = TRUE))
 
 
 # Bulgaria
 bul_birth2 <- bul_birth %>% 
   filter(OpenInterval == FALSE) %>% 
-  group_by(Year) %>% 
+  group_by(Year, Age) %>% 
   summarise(Births = sum(Total, na.rm = TRUE))
 
 bul_exposure2 <- bul_exposure %>% 
   filter(OpenInterval == FALSE) %>% 
-  group_by(Year) %>% 
+  group_by(Year, Age) %>% 
   summarise(Exposure = sum(Exposure, na.rm = TRUE))
 
 # Korea
 kor_birth2 <- kor_birth %>% 
   filter(OpenInterval == FALSE) %>% 
-  group_by(Year) %>% 
+  group_by(Year, Age) %>% 
   summarise(Births = sum(Total, na.rm = TRUE))
 
 kor_exposure2 <- kor_exposure %>% 
   filter(OpenInterval == FALSE) %>% 
-  group_by(Year) %>% 
+  group_by(Year, Age) %>% 
   summarise(Exposure = sum(Exposure, na.rm = TRUE))
 
 
 
 
 # All together now
-spain <- spain_exposure2 %>% left_join(spain_birth2, by = "Year")
+spain <- spain_exposure2 %>% left_join(spain_birth2, by = c("Year", "Age"))
 
-bulgaria <- bul_exposure2 %>% left_join(bul_birth2, by = "Year")
+bulgaria <- bul_exposure2 %>% left_join(bul_birth2, by = c("Year", "Age"))
 
-korea <- kor_exposure2 %>% left_join(kor_birth2, by = "Year")
+korea <- kor_exposure2 %>% left_join(kor_birth2, by = c("Year", "Age"))
 
 # Deleting what we don't use anymore
 rm(spain_birth, spain_birth2,
@@ -102,6 +102,29 @@ rm(spain_birth, spain_birth2,
    bul_exposure, bul_exposure2,
    kor_birth, kor_birth2,
    kor_exposure, kor_exposure2)
+
+# All together in 1 data.frame
+spain$cty <- "Spain"
+bulgaria$cty <- "Bulgaria"
+korea$cty <- "Korea"
+
+challenge2 <- rbind(spain, bulgaria, korea)
+
+
+
+# Some plotting
+challenge2 %>% 
+  group_by(cty, Year) %>% 
+  summarise(Total_Birth = sum(Births, na.rm = T),
+            Total_Exposure = sum(Exposure, na.mr = T)) %>% 
+  mutate(TFR = Total_Birth/Total_Exposure) %>% 
+  filter(Year >= 2000) %>% 
+  ggplot(aes(x=Year, y = TFR, color = cty)) + geom_line()
+
+
+
+
+
 
 
 
